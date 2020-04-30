458ad34e94a1b74cd4a7b9e653d7e7621a87c5cf
Ruchit Parikh <ruchitparikh17@gmail.com>
2020-04-30 23:27:46 +0530

    Updates

---
+++ precache-manifest.6a41154a1ae45e316b4708aa0e50fbb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d3c4f51ca8e43b02055921847d8271",
+    "url": "/simple-react-game/index.html"
+  },
+  {
+    "revision": "2fe31879a61ef51f037f",
+    "url": "/simple-react-game/static/css/main.2fa5121c.chunk.css"
+  },
+  {
+    "revision": "f6b099a38d5a9820d664",
+    "url": "/simple-react-game/static/js/2.eda7be6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-game/static/js/2.eda7be6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe31879a61ef51f037f",
+    "url": "/simple-react-game/static/js/main.16efc199.chunk.js"
+  },
+  {
+    "revision": "2f9de6a1ec91570aa541",
+    "url": "/simple-react-game/static/js/runtime-main.9a245f8e.js"
+  }
+]);
